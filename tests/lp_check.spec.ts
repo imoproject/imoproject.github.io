c185f4d075e3266123cd6361cb7bd0a96bd50eca
imofessional <kazuki0623tennis@gmail.com>
2025-07-17 00:42:11 +0900

    テスト修正

--- tests/lp_check.spec.ts
+++ tests/lp_check.spec.ts
@@ -2,5 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('トップページが表示される', async ({ page }) => {
   await page.goto('http://localhost:8080/'); // HTMLを表示するURL
-  await expect(page).toHaveTitle(/My Site|Example/); // title に応じて調整
+  await expect(page).toHaveTitle(/IMOPROJECT HOME|Example/); // title に応じて調整
 });
