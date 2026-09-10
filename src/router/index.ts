11d88bdab45cd50af7732c2e2f7733204cd122ce
koaranoma-chi0530 <kanako03@g.ecc.u-tokyo.ac.jp>
2026-09-10 11:00:12 +0900

    fix: support GitHub Pages routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import Activity from "../views/Activity.vue";
 import Faq from "../views/Faq.vue";
@@ -24,7 +24,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
